497b96fd168fa5024f868a6e73da773859e5f944
shay-siri <102751610+shay-siri@users.noreply.github.com>
2022-05-22 17:32:24 -0700

    Shay

---
+++ js/index.js
@@ -0,0 +1,10 @@
+* Author:    Shay, Annalivia, Brent, Tamara, and Kenny
+* Created:   05.22.2022
+*
+* (c) Copyright by Project Climate corp.
+**/
+
+const canvas = document.querySelector("canvas");
+
+canvas.width = 64
+canvas.height = 64
