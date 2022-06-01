9ce088c791e909f410b80167c7c94769e7c38bfb
tamgerine <66058736+tamgerine@users.noreply.github.com>
2022-06-01 13:31:59 -0700

    working on text parsing

--- scripts/game.js
+++ scripts/game.js
@@ -8,12 +8,12 @@ var currentRoom; //keeps track of which room we are in
 //sizes the canvas based on window size
 //i would just collapse this if i were you bc oh god why
 function sizeCanvas(){
-  var header = $("#header"); //use jQuery to get header
-  var footer = $("#footer"); //use jQuery to get footer
-  var inHeight = innerHeight;//innerHeight of the window
-  var inWidth = innerWidth; //innerWidth of the window
-  var headerHeight = header.outerHeight(true); //height of our header
-  var footerHeight = footer.outerHeight(true); //height of our header
+  let header = $("#header"); //use jQuery to get header
+  let footer = $("#footer"); //use jQuery to get footer
+  let inHeight = innerHeight;//innerHeight of the window
+  let inWidth = innerWidth; //innerWidth of the window
+  let headerHeight = header.outerHeight(true); //height of our header
+  let footerHeight = footer.outerHeight(true); //height of our header
   if((inHeight-headerHeight-footerHeight)<=inWidth){ //if the height is less than the width
     remainder = (inHeight-headerHeight-footerHeight)%64;// do this math
     canvas.width = inHeight-headerHeight-footerHeight-remainder;//and then set the width using math
@@ -37,7 +37,7 @@ function sizeCanvas(){
   textAreaY3 = (canvas.height/64)*50;
   textAreaY4 = (canvas.height/64)*54;
 
-  var fontsize = Math.trunc((canvas.height/64)*3);//quick maths
+  let fontsize = Math.trunc((canvas.height/64)*3);//quick maths
   ctx.font = fontsize+"px pixelFont"; // set font
   ctx.textBaseline = "top"; //set
 }
@@ -74,8 +74,8 @@ text_box.src = 'images/text_box.png';
 // YIKES
 var playerSize = canvas.width/16;//playerSize in pixels
 
-var levelCols=8;// level width, in tiles
-var levelRows=8; // level height, in tiles
+const levelCols=8;// level width, in tiles
+const levelRows=8; // level height, in tiles
 var tileSize= canvas.width/8; // tile size, in pixels
 var playerCol=1;// player starting column
 var playerRow=3; // player starting row
@@ -94,7 +94,11 @@ var textAreaY1 = (canvas.height/64)*42;
 var textAreaY2 = (canvas.height/64)*46;
 var textAreaY3 = (canvas.height/64)*50;
 var textAreaY4 = (canvas.height/64)*54;
-
+var shownString = "";
+var hiddenString = "";
+var pageCount = 1;
+var currentPage = 1;
+var frameCount = 0;
 
 
 //room object template
@@ -131,7 +135,7 @@ function Item(image, text, interacted, row, col, walkable) {
 }
 //this is a test
 var blackSquare = new Item();
-blackSquare.text = "this is a nice big long string so you can do all your fun operations on it";
+blackSquare.text = "this is a nice big long string so you can do all your fun operations on it bla bla bla hey bla bla bla hey bla bla bla hey bla bla bla hey look bla bla bla wow you made it to the very very end of the string!";
 blackSquare.row =3;
 blackSquare.col =4;
 //this is a test too
@@ -157,9 +161,10 @@ function loop() {
 
 // NOT DONE
 //draws text box and text
-function drawText(){
+function drawText(pageStr){
   ctx.drawImage(text_box, 0, 0, canvas.width,canvas.height);
-  ctx.fillText("help", textAreax,textAreaY1);
+  //here were gonna seperate the string which already has /n in for the lines, this only takes the string for one "page" on the text box
+  ctx.fillText("help\nhelp", textAreax,textAreaY1);
   ctx.fillText("help", textAreax,textAreaY2);
   ctx.fillText("help", textAreax,textAreaY3);
   ctx.fillText("help", textAreax,textAreaY4);
@@ -193,8 +198,9 @@ function draw() {
   ctx.fillText(playerDirection,playerXPos+tileSize*.25,playerYPos+tileSize*.25);
   //check if player is in dialogue and draw text
   if (inDialogue) {
-    drawText();
+    drawText(shownString);
   }
+  frameCount++;
 }
 
 
@@ -249,19 +255,20 @@ function update() {
     movePlayer();
   }
   //if the player is done moving then do the updating
+
   else{
+    let fasterText = false;
     if(spacebarPressed){
+      formatText("Have you ever typed a few dashes in between paragraphs (as a placeholder or whatever), hit enter, and somehow wound up with a line all the way across the page that you can't get rid of, no matter how many times you hit the Delete key?");
 
       if(!inDialogue){
         //check if the player is standing on interactable tile
         if(currentRoom.map[playerRow][playerCol]==2){
           interact(currentRoom.items.find( (ite) => ite.row ==playerRow&&ite.col==playerCol));
         }
-        //do something
-
 
         //check if the player is facing an interactable (non walkable) tile
-        if (( playerDirection == 'e' )&&(currentRoom.map[playerRow][playerCol+1]==3)) {
+        else if (( playerDirection == 'e' )&&(currentRoom.map[playerRow][playerCol+1]==3)) {
           //call the interact function on the item in the proper position
           interact(currentRoom.items.find( (ite) => ite.row ==playerRow&&ite.col==(playerCol+1)));
         } else if(( playerDirection == 'w' )&&(currentRoom.map[playerRow][playerCol-1]==3)){
@@ -271,17 +278,24 @@ function update() {
         } else if (( playerDirection == 's' )&&(currentRoom.map[playerRow+1][playerCol]==3)) {
           interact(currentRoom.items.find( (ite) => ite.row ==(playerRow+1)&&ite.col==playerCol));
         }
-        //do something
-
 
         //if we only want the thing to be interactable once, update the space to 0 or 1
 
 
-      }else {
-        advanceText();
+      }else if(inDialogue){
+        fasterText=advanceText();
       }
     }
-    if(rightPressed){
+
+    if (inDialogue) {
+      if(true){//condition for this is if the character at the top of the hidden string is not the new page character
+        if(!fasterText){
+          //move one letter from hiddenString to shownString
+        }else {
+          //move one letter from hiddenString to shownString but faster
+        }
+      }
+    }else if(rightPressed){
       playerDirection='e';
       if (isPathTile(playerRow,playerCol+1)) {
         playerCol+=1;
@@ -354,14 +368,50 @@ function movePlayer(){
 // NOT DONE
 //interacts with the specified item
 function interact(item) {
+  inDialogue=true;
+  currentPage = 1;
+  pageCount = formatText(item.text);
   console.log(item.text);
+
   //if we only want the thing to be interactable once, update the space to 0 or 1
 }
-
+//takes a string, adds newlines and carriage returns where lines and pages should end so that they fit in the text box
+function formatText(string){
+  let lines=1;
+  let pages=1;
+  let startingIndex=0;
+  for(let i=0; i< string.length; i++){
+    let builder = "";
+    if(ctx.measureText(string.substring(startingIndex,i))>=((canvas.width/64)*47)){
+      if((lines%4)==0){
+        builder = string.slice(startingIndex,i)+"\r";
+        startingIndex=i+1;
+        lines++;
+        pages++;
+      }
+    }else if (ctx.measureText(string.substring(startingIndex,i))>=((canvas.width/64)*51)) {
+      builder = string.slice(startingIndex,i)+"\n";
+      startingIndex=i+1;
+      lines++;
+      pages++;
+    }
+    hiddenString.concat(builder);
+  }
+  console.log(hiddenString);
+  return pages;
+}
 // NOT DONE
 function advanceText() {
-  //not sure what to do here but will ponder it
-  inDialogue=false;
+  //if hiddenString char at 0 is equal to newpage char then set shownstring to empty and increment currentPage
+
+  if (hiddenString=="") {
+    shownString="";
+    currentPage=1;
+    pageCount=1;
+    inDialogue=false;
+
+  }
+  return true;
 }
 
 // Refreshes State, so site doesn't crash (Calls Loop function every 1000/30 milliseconds(30fps))
